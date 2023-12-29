bee73726f0eb9d721d494fa645c69c4915e3c3b4
ItaSaJu <italosalas63@gmail.com>
2023-12-29 19:41:13 +0100

    feat: agregando componente garantizamos

--- src/app/features/homepage/garantia/garantia.component.ts
+++ src/app/features/homepage/garantia/garantia.component.ts
@@ -5,7 +5,7 @@ import { TranslateModule } from '@ngx-translate/core';
 @Component({
   selector: 'app-garantia',
   templateUrl: './garantia.component.html',
-  styleUrls: ['./garantia.component.css'],
+  styleUrls: ['./garantia.component.scss'],
   standalone: true,
   imports:[
     CommonModule ,
@@ -14,4 +14,34 @@ import { TranslateModule } from '@ngx-translate/core';
 })
 export class GarantiaComponent {
 
+  
+
+
+  card:any[]=[
+    {
+      "img": "../../../../assets/img/documentation.svg",
+      "title": "Reporte de toma de decisione",
+      "description": "Informes para análisis gerencial que ofrecen una visión completa del negocio, permitiendo decisiones informadas basadas en datos, identificando patrones y oportunidades de mejor."
+    },
+    {
+      "img": "../../../../assets/img/ui.svg",
+      "title": "Ui/Ux Design",
+      "description": "El diseño de experiencia de usuario (UX) es clave para la eficacia y satisfacción del usuario, mejorando retención, eficiencia y reduciendo costos operativos."
+    },
+    {
+      "img": "../../../../assets/img/html.svg",
+      "title": "Web development",
+      "description": "Desarrollo web experto para sitios y aplicaciones, enfocado en eficiencia y satisfacción del usuario para potenciar tu presencia digital."
+    },
+    {
+      "img": "../../../../assets/img/mobile.svg",
+      "title": "App development",
+      "description": "Expertos en desarrollo de aplicaciones: creamos soluciones eficientes y centradas en la experiencia del usuario para potenciar tu presencia digital móvil."
+    },
+    {
+      "img": "../../../../assets/img/bolas.svg",
+      "title": "Analisis de procesos",
+      "description": "Diferenciamos con enfoque analítico, pasión por resolver problemas operativos y soluciones de software personalizadas. Impulsamos tu éxito y destacamos en el mercado con eficiencia mejorada."
+    }
+  ]
 }
